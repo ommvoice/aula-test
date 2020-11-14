400d449866cc3cd50ee1b87ddfb4d52273b02859
ommvoice <holypulse@gmail.com>
2020-11-14 01:31:42 +0000

    Fixture and more unit tests added

---
+++ src/mocks/fixtures/songs.js
@@ -0,0 +1,14 @@
+export const SONGS = {
+  feed: {
+    results: [{
+      id: "1",
+      artistName: "Kodak Black",
+      name: "Bill Israel",
+    }, {
+      id: "2",
+      artistName: "Anonymous",
+      name: "Babra",
+    }],
+  }
+};
+  
--- src/services/api/__tests__/songs.spec.js
+++ src/services/api/__tests__/songs.spec.js
@@ -1,15 +1,23 @@
 import axios from 'axios';
 import { songsApi } from '../songs';
+import { SONGS } from '../../../mocks/fixtures/songs';
  
 jest.mock('axios');
  
 describe('Axios', () => { 
-	it('fetches successfully data from an API', async () => {
-		const category = 'top-albums';
+	const category = 'top-albums';
+
+	it('calls axios with correct url', async () => {
 		songsApi(category);
 
 		expect(axios.request).toHaveBeenCalledWith(
 			{ url: `https://rss.itunes.apple.com/api/v1/us/apple-music/${category}/all/100/non-explicit.json` }
 		);
+	});
+
+	it('fetches successfully data from an API', async () => {	 
+		axios.request.mockImplementationOnce(() => Promise.resolve(SONGS));
+	 
+		await expect(songsApi('react')).resolves.toEqual(SONGS);
 	});
 });
